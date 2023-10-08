a5fd1e12feeb7fd1503b26d674d986516e057bc1
Azzy <95762556+AzzyB@users.noreply.github.com>
2023-10-08 14:46:45 -0400

    Bug Fixes/Feature updates
    * Encryption/Decryption through gnupg are password based.
    * Added feature to remove previous file after generating a new file from most option outputs.
    * Fixed verbose when entering filenames to help user understand usage in real time.
    * Fixed bugs with gpg tool not working properly

--- comprezy.sh
+++ comprezy.sh
@@ -2,16 +2,18 @@
 
 # FILE: Comprezy
 # USAGE: ./comprezy.sh
-# DESC: Comprezy is just a simple temrinal based tool to help with the use of compression tools like gz and xz
+#
+# DESC: Comprezy is just a simple terminal based tool to help with the use of compression tools like gz and xz along with
+# gnupg encryption to manipulate file archiving
+#
 # CREATED: by Azzy on 10/4/2023
-# VERSION: 1.4.1
-# 
+# VERSION: 1.6.3
 
 #Function to check if a package is installed and install it if not
 checkPackage() {
 
 	local packageName="$1"
-	
+
 	if ! command -v "$packageName" &>/dev/null; then
 		echo "$packageName is not installed. Attempting to install..."
 
@@ -24,141 +26,12 @@ checkPackage() {
 
 		elif command -v apt-get &>/dev/null; then
 			sudo apt-get install -y "$packageName"
-			
+
 		else
 			echo "Unsupported package manager. Please install $packageName manually."
             exit 1
         fi
     fi
-}
-
-#Function to compress files/directories using tar and gzip
-compressWith_gz() {
-
-    checkPackage "tar"
-    checkPackage "gzip"
-    
-    read -p "Enter the path to the file/directory: " sourcePath
-    read -p "Enter the desired name for the compressed file: " compressedName
-    
-    # Add .tar.gz extension to the user's input
-    compressedName="${compressedName}.tar.gz"
-    
-    # Create the compressed file
-    tar -czvf "$compressedName" "$sourcePath"
-    
-    read -p "Do you want to delete the original files (y/n)? " deleteOriginal
-	
-    if [ "$deleteOriginal" == "y" ]; then
-	
-		rm -r "$sourcePath"
-		
-    fi
-}
-
-# Function to compress files/directories using tar and xz
-compressWith_xz() {
-
-    checkPackage "tar"
-    checkPackage "xz"
-    
-    read -p "Enter the path to the file/directory: " sourcePath
-    read -p "Enter the desired name for the compressed file: " compressedName
-    
-    # Add .tar.xz extension to the user's input
-    compressedName="${compressedName}.tar.xz"
-    
-    # Create the compressed file
-    tar -cvf - "$sourcePath" | xz -zv > "$compressedName"
-    
-    read -p "Do you want to delete the original files (y/n)? " deleteOriginal
-	
-    if [ "$deleteOriginal" == "y" ]; then
-	
-        rm -r "$sourcePath"
-		
-    fi
-}
-
-# Function to uncompress and unarchive files
-uncompress() {
-    read -p "Enter the compressed filename (with .tar.gz or .tar.xz extension): " compressedFile
-    read -p "Enter the desired uncompressed filename: " uncompressedName
-    
-    if [[ "$compressedFile" == *.tar.gz ]]; then
-        checkPackage "gzip"
-        tar -xzvf "$compressedFile" -C "$(dirname "$uncompressedName")"
-    elif [[ "$compressed_file" == *.tar.xz ]]; then
-        checkPackage "xz"
-        tar -xJvf "$compressedFile" -C "$(dirname "$uncompressedName")"
-    else
-        echo "Unsupported compression format. Please use .tar.gz or .tar.xz files."
-    fi
-
-}
-
-# Function to encrypt a file
-encryptFile() {
-
-    checkPackage "gpg"
-
-    read -p "Enter the filename to encrypt: " file_to_encrypt
-
-    if [ "$passphrase" != "$confirm_passphrase" ]; then
-
-        echo "Passphrases do not match. Encryption canceled."
-        return
-
-    fi
-
-    gpg -c --passphrase "$passphrase" "$file_to_encrypt"
-    echo "File encrypted successfully."
-
-}
-
-# Function to decrypt an encrypted file
-decryptFile() {
-
-    checkPackage "gpg"
-    read -p "Enter the encrypted filename: " encrypted_file
-    echo
-
-    gpg --batch --yes --passphrase "$passphrase" -d "$encrypted_file" > decrypted_file
-    echo "File decrypted successfully."
-
-}
-
-# Function to create a tar archive
-archiveWith_tar() {
-
-    checkPackage "tar"
-    
-    read -p "Enter the path to the file/directory: " sourcePath
-    read -p "Enter the desired name for the archive: " archiveName
-    
-    # Add .tar extension to the user's input
-    archiveName="${archiveName}.tar"
-    
-    # Create the tar archive
-    tar -cvf "$archiveName" "$sourcePath"
-    
-    read -p "Do you want to delete the original files (y/n)? " deleteOriginal
-	
-    if [ "$deleteOriginal" == "y" ]; then
-	
-        rm -r "$sourcePath"
-		
-    fi
-}
-
-# Function to set the default directory in Settings for compressed files 
-setDefaultDirectory() {
-
-	read -p "Enter the default directory path: " default_dir
-	echo "DEFAULT_DIR=\"$default_dir\"" > settings.conf
-	echo "Default directory is set to: $default_dir"
-	read -p "Press Enter to continue..."
-
 }
 
 # Function to update xz and gzip packages
@@ -168,6 +41,175 @@ updatePackages() {
     checkPackage "gzip"
     echo "xz and gzip packages are up to date."
     read -p "Press Enter to continue..."
+
+}
+
+#Function to compress files/directories using tar and gzip
+compressWith_gz() {
+
+    checkPackage "tar"
+    checkPackage "gzip"
+
+    echo "Note: Enter the full file name including extension or full directory path unless the file/folder is in the current working directory."
+    read -p "Enter the path to the file/directory: " sourcePath
+
+    echo "Note: The compression will auto append .tar.gz to the output."
+    read -p "Enter the desired name for the compressed file: " compressedName
+
+    # Add .tar.gz extension to the user's input
+    compressedName="${compressedName}.tar.gz"
+
+    # Create the compressed file
+    tar -czvf "$compressedName" "$sourcePath"
+
+    read -p "Do you want to delete the original files (y/n)? " deleteOriginal
+
+    if [ "$deleteOriginal" == "y" ]; then
+
+		rm -r "$sourcePath"
+
+    fi
+}
+
+# Function to compress files/directories using tar and xz
+compressWith_xz() {
+
+    checkPackage "tar"
+    checkPackage "xz"
+
+    echo "Note: Enter the full file name including extension or full directory path unless the file/folder is in the current working directory."
+    read -p "Enter the path to the file/directory: " sourcePath
+
+    echo "Note: The compression will auto append .tar.xz to the output."
+    read -p "Enter the desired name for the compressed file: " compressedName
+
+    # Add .tar.xz extension to the user's input
+    compressedName="${compressedName}.tar.xz"
+
+    # Create the compressed file
+    tar -cvf - "$sourcePath" | xz -zv > "$compressedName"
+
+    read -p "Do you want to delete the original files (y/n)? " deleteOriginal
+
+    if [ "$deleteOriginal" == "y" ]; then
+
+        rm -r "$sourcePath"
+
+    fi
+}
+
+# Function to uncompress and unarchive files
+uncompress() {
+    read -p "Enter the compressed filename (with .tar.gz or .tar.xz extension): " compressedFile
+    checkPackage "gzip"
+    checkPackage "xz"
+
+    if [[ "$compressedFile" == *.tar.gz ]]; then
+
+        tar -xzvf "$compressedFile"
+
+        read -p "Do you want to delete the original archive (y/n)? " deleteOriginal
+
+            if [ "$deleteOriginal" == "y" ]; then
+
+                rm -r "$compressedFile"
+
+            fi
+
+    elif [[ "$compressedFile" == *.tar.xz ]]; then
+
+        tar -xJvf "$compressedFile"
+
+        read -p "Do you want to delete the original archive (y/n)? " option
+
+            if [ "$option" == "y" ]; then
+
+                rm -r "$compressedFile"
+
+            fi
+
+    else
+
+        echo "Unsupported compression format. Please use .tar.gz or .tar.xz files."
+
+    fi
+
+}
+
+# Function to encrypt a file
+encryptFile() {
+
+    checkPackage "gpg"
+
+    read -p "Enter the filename to encrypt: " fileToEncrypt
+
+    gpg --symmetric --cipher-algo AES256 --armor -o "$fileToEncrypt".gpg "$fileToEncrypt"
+
+    echo "File encrypted successfully."
+
+    read -p "Do you want to delete the original archive (y/n)? " option
+
+    if [ "$option" == "y" ]; then
+
+        rm -r "$fileToEncrypt"
+
+    fi
+
+}
+
+# Function to decrypt an encrypted file
+decryptFile() {
+
+    checkPackage "gpg"
+    read -p "Enter the encrypted filename: " encryptedFile
+    echo
+
+    decryptedFile="${encryptedFile%????}"
+
+    gpg -d -o "$decryptedFile" "$encryptedFile"
+
+    echo "File decrypted successfully."
+
+    read -p "Do you want to delete the original encrypted file? (y/n)? " option
+
+    if [ "$option" == "y" ]; then
+
+        rm -r "$encryptedFile"
+
+    fi
+
+}
+
+# Function to create a tar archive
+archiveWith_tar() {
+
+    checkPackage "tar"
+
+    read -p "Enter the path to the file/directory: " sourcePath
+    read -p "Enter the desired name for the archive: " archiveName
+
+    # Add .tar extension to the user's input
+    archiveName="${archiveName}.tar"
+
+    # Create the tar archive
+    tar -cvf "$archiveName" "$sourcePath"
+
+    read -p "Do you want to delete the original files (y/n)? " deleteOriginal
+
+    if [ "$deleteOriginal" == "y" ]; then
+
+        rm -r "$sourcePath"
+
+    fi
+}
+
+# Function to set the default directory in Settings for compressed files
+setDefaultDirectory() {
+
+	read -p "Enter the default directory path: " default_dir
+	echo "DEFAULT_DIR=\"$default_dir\"" > settings.conf
+	echo "Default directory is set to: $default_dir"
+	read -p "Press Enter to continue..."
 
 }
 
@@ -197,18 +239,18 @@ while true; do
     echo "7. Settings"
     echo "8. Exit"
     echo " "
-		
+
     read -p "Choose an Option: " choice
-	
+
     case $choice in
-	
+
         1) compressWith_gz ;;
         2) compressWith_xz ;;
 	    3) uncompress ;;
         4) encryptFile ;;
         5) decryptFile ;;
         6) archiveWith_tar ;;
-        7) 
+        7)
 
 		clear
             	echo "-=-=-=- Settings -=-=-=-"
@@ -226,9 +268,7 @@ while true; do
             	esac ;;
         8) exit 0 ;;
         *) echo "Invalid choice. Please select a valid option." ;;
-		
+
     esac
-	
-    read -p "Press Enter to continue..."
-	
+
     done
